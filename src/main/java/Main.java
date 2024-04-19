78bea1673b2870fe6f43a216b9fda9598a10e409
Leo Proger <leonidkrasavin4@gmail.com>
2024-04-19 14:50:50 +0300

    Init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+package SelectSort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+
+        array.add(5);
+        array.add(3);
+        array.add(4);
+        array.add(1);
+
+        ArrayList<Integer> sortedArray = sortArray(array);
+        System.out.println(sortedArray);
+    }
+
+    private static ArrayList<Integer> sortArray(ArrayList<Integer> array) {
+        if (array.size() < 2) return array;
+        else if (array.size() == 2) {
+            ArrayList<Integer> tmp = new ArrayList<>();
+
+            tmp.add(Collections.min(array));
+            tmp.add(Collections.max(array));
+
+            return tmp;
+        }
+
+        int pivot = array.get(array.size() / 2);
+
+        ArrayList<Integer> less = new ArrayList<>();
+        for (int el : array)
+            if (el < pivot)
+                less.add(el);
+
+        ArrayList<Integer> more = new ArrayList<>();
+        for (int el : array)
+            if (el > pivot)
+                more.add(el);
+
+
+        ArrayList<Integer> sortedLess = sortArray(less);
+        sortedLess.add(pivot);
+
+        ArrayList<Integer> sortedMore = sortArray(more);
+        sortedLess.addAll(sortedMore);
+
+        return sortedLess;
+    }
+}
+
